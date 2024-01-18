ede76083a7a84fa527d473fa0e3d4a9aea7c56b4
AbsintheSung <h90243768@gmail.com>
2024-01-18 10:14:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+
+</script>
+
+<template>
+
+  <header class="container-fluid">
+
+  </header>
+
+  <div class="container-xl">
+    <div class="row">
+
+      <aside class="col-2">
+        <ul class="sidebar-content-scroll">
+          <li><a>创建一个应用</a></li>
+          <li><a>创建一个应用</a></li>
+          <li><a>创建一个应用</a></li>
+          <li><a>创建一个应用</a></li>        
+          <li><a>创建一个应用測試用</a></li> 
+        </ul>
+      </aside>
+
+      <main class="col-10">
+        <div class="main"></div>
+
+        <RouterView />
+
+      </main>
+
+    </div>
+   
+
+
+    
+  </div>
+
+
+
+</template>
+
+<style lang="scss" scoped>
+
+aside{
+  height: 100vh;
+
+}
+.sidebar-content-scroll{
+  height:100%;
+  position: fixed;
+  overflow-y: auto; /* 允許滾動 */
+  overflow-x: hidden;
+  >li{
+    margin-right: 20px;
+  }
+
+}
+.main{
+  /* height: 200vh; */
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+ <button type="button" class="btn btn-primary-2">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+</template>
+
+<style lang="scss" scoped>
+// @import "../assets/scss/all.scss"
+
+
+</style>
